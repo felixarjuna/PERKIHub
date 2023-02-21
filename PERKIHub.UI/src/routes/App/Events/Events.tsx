7f9f816ad8379356d3d1d9b5ca3c8ee6192d9cac
felixarjuna <felixarjuna.de@yahoo.com>
2023-02-21 21:16:23 +0100

    🐹 Update Event Card and Events Page

--- PERKIHub.UI/src/routes/App/Events/Events.tsx
+++ PERKIHub.UI/src/routes/App/Events/Events.tsx
@@ -4,23 +4,31 @@ import { EventCard } from '../Cards/EventCard';
 
 export const Events = () => {
   const navigate = useNavigate();
+
   return (
     <div>
       <div className="mt-10">
-        <h1 className="text-6xl text-lightmaroon">Events</h1>
+        <h1 className="text-6xl text-gradient">Events</h1>
         <h3 className="mt-10 text-4xl">This Week</h3>
       </div>
 
-      <div
-        className="mt-4 flex items-center gap-3 cursor-pointer"
-        onClick={() => navigate('create')}
-      >
-        <BsFillCalendarPlusFill />
-        <p>New event</p>
+      <div className="mt-7 w-44 bg-lightmaroon">
+        <div
+          className="mt-4 flex items-center gap-3 cursor-pointer button-cream w-44 justify-center"
+          onClick={() => navigate('create')}
+        >
+          <BsFillCalendarPlusFill />
+          <p>New event</p>
+        </div>
       </div>
 
       <div>
-        <EventCard />
+        <EventCard
+          title="Saturday Service"
+          date={new Date()}
+          speaker="Pdt. John Kusuma"
+          topic="Time Management"
+        />
       </div>
     </div>
   );
