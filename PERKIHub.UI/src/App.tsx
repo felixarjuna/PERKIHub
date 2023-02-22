6f9521b920ec6aa1de540a5db7e5e706847cc68e
felixarjuna <felixarjuna.de@yahoo.com>
2023-02-22 22:19:02 +0100

    🌭 Enable Login in Home Page

--- PERKIHub.UI/src/App.tsx
+++ PERKIHub.UI/src/App.tsx
@@ -1,4 +1,5 @@
 import { CgProfile } from 'react-icons/cg';
+import { FiLogIn } from 'react-icons/fi';
 import { NavLink, Outlet } from 'react-router-dom';
 import { useAuth } from './lib/hooks/useAuth';
 
@@ -9,12 +10,23 @@ function App() {
   return (
     <div className="mx-16 my-5 p-10">
       <div className="flex justify-end items-center gap-3">
-        <h3 className="text-2xl">
-          Welcome, <span>{user?.currentUser ? name : 'User'}</span>!
-        </h3>
-        <NavLink to={'/profile'} className="text-3xl">
-          <CgProfile />
-        </NavLink>
+        {user?.currentUser ? (
+          <>
+            <h3 className="text-2xl">
+              Welcome, <span>{user?.currentUser ? name : 'User'}</span>!
+            </h3>
+            <NavLink to={'/profile'} className="text-3xl">
+              <CgProfile />
+            </NavLink>
+          </>
+        ) : (
+          <div className="flex gap-10 text-2xl">
+            <NavLink to="/login" className="text-cream flex items-center gap-1">
+              Login
+              <FiLogIn />
+            </NavLink>
+          </div>
+        )}
       </div>
 
       <div>
