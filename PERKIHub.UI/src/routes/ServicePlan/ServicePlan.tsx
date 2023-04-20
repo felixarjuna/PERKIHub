2e93804e0dcbab7a9184ece60d81c6208d06ce8c
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-04-20 19:51:50 +0200

    ❤️ Add ServiceGroups

--- PERKIHub.UI/src/routes/ServicePlan/ServicePlan.tsx
+++ PERKIHub.UI/src/routes/ServicePlan/ServicePlan.tsx
@@ -3,20 +3,20 @@ import { groupList } from "./data";
 export const ServicePlan = () => {
   return (
     <div className="p-8 sm:px-20 text-center">
-      <div className="text-6xl text-gradient-soft">
+      <div className="text-4xl font-bold sm:text-6xl text-gradient-soft">
         Cooking and Cleaning Groups
       </div>
 
-      <div className="flex justify-center">
-        <div className="sm:mt-16 flex gap-16 w-1/2 flex-wrap">
+      <div className="mt-10 flex justify-center">
+        <div className="sm:mt-16 flex gap-16 w-1/2 flex-wrap justify-center">
           {groupList.map(g => (
             <div className="w-60">
               <div className="bg-lightmaroon">
-                <div className="button-cream w-60 text-center -translate-x-1 -translate-y-1 text-2xl">
+                <div className="button-cream w-60 text-center -translate-x-1 -translate-y-1 text-xl sm:text-2xl">
                   <p>{g.Name}</p>
                 </div>
               </div>
-              <ul className="mt-4 text-center text-xl font-extralight flex flex-col gap-3 font-unbounded">
+              <ul className="mt-4 text-center  sm:text-xl font-extralight flex flex-col gap-3 font-unbounded">
                 {g.Members.map(m => (
                   <li>{m}</li>
                 ))}
