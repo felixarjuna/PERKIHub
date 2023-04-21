4a3e69433cb902059dc04ca720e1b92efcd009ea
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-04-21 05:25:47 +0200

    🚗 Add Loading Spinner

--- PERKIHub.UI/src/routes/App/Events/Events.tsx
+++ PERKIHub.UI/src/routes/App/Events/Events.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import {
   BsExclamationTriangleFill,
   BsFillCalendarPlusFill,
@@ -15,55 +16,64 @@ export const Events = () => {
 
   const navigate = useNavigate();
 
-  const { events } = useEvents("");
+  const { events, isLoading } = useEvents();
 
   return (
-    <div className="p-8 sm:px-20">
-      <div className="sm:mt-10 mb-5">
-        <h1 className="text-3xl sm:text-6xl text-gradient-soft font-bold sm:font-normal tracking-wide">
-          Events
-        </h1>
-      </div>
-
-      {AUTHORIZED_USERS.includes(name) ? (
-        <div className="my-7 w-36 sm:w-44 bg-lightmaroon">
-          <div
-            className="mt-4 flex items-center gap-3 cursor-pointer button-cream w-36 sm:w-44 justify-center"
-            onClick={() => navigate("create")}
-          >
-            <BsFillCalendarPlusFill />
-            <p>New event</p>
-          </div>
+    <>
+      {isLoading && (
+        <div className="flex gap-3 items-center justify-center bg-tundora-800 w-screen h-screen z-10 fixed inset-0">
+          <AiOutlineLoading3Quarters className="animate-spin" />
+          <p>Loading Content</p>
         </div>
-      ) : (
-        <></>
       )}
 
-      <div className="my-6 sm:m-0 sm:mt-10">
-        <div className="warning-component text-sm sm:text-xl items-center gap-2 inline-flex">
-          <BsExclamationTriangleFill />
-          <p className="">All events can only joined one day before.</p>
+      <div className="p-8 sm:px-20">
+        <div className="sm:mt-10 mb-5">
+          <h1 className="text-4xl sm:text-6xl text-gradient-soft font-bold sm:font-normal tracking-wide">
+            Events
+          </h1>
+        </div>
+
+        {AUTHORIZED_USERS.includes(name) ? (
+          <div className="my-7 w-36 sm:w-44 bg-lightmaroon">
+            <div
+              className="mt-4 flex items-center gap-3 cursor-pointer button-cream w-36 sm:w-44 justify-center"
+              onClick={() => navigate("create")}
+            >
+              <BsFillCalendarPlusFill />
+              <p>New event</p>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+
+        <div className="my-6 sm:m-0 sm:mt-10">
+          <div className="warning-component text-sm sm:text-xl items-center gap-2 inline-flex">
+            <BsExclamationTriangleFill />
+            <p className="">All events can only joined one day before.</p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap gap-7 justify-center sm:justify-start">
+          {events
+            ?.sort((a, b) => {
+              if (a.date > b.date) return -1;
+              return 1;
+            })
+            .map(event => (
+              <EventCard
+                key={event.id}
+                id={event.id}
+                title={event.title}
+                date={event.date}
+                speaker={event.speaker}
+                topic={event.topic}
+                participants={event.participants}
+              />
+            ))}
         </div>
       </div>
-
-      <div className="flex flex-wrap gap-7 justify-center sm:justify-start">
-        {events
-          ?.sort((a, b) => {
-            if (a.date > b.date) return -1;
-            return 1;
-          })
-          .map(event => (
-            <EventCard
-              key={event.id}
-              id={event.id}
-              title={event.title}
-              date={event.date}
-              speaker={event.speaker}
-              topic={event.topic}
-              participants={event.participants}
-            />
-          ))}
-      </div>
-    </div>
+    </>
   );
 };
