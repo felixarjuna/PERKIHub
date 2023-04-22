b7164e212759766e5d258418726ddec1bcd5c1d0
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-04-22 15:27:25 +0200

    🥙 Connect Migrations to SQL Server

--- backend/PERKIHub.RestApi/Migrations/20230422131158_UpdateTable.Designer.cs
+++ backend/PERKIHub.RestApi/Migrations/20230422131158_UpdateTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PERKIHub.RestApi.Persistence;
 
@@ -11,9 +12,11 @@ using PERKIHub.RestApi.Persistence;
 namespace PERKIHub.RestApi.Migrations
 {
     [DbContext(typeof(PerkiHubDbContext))]
-    partial class PerkiHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230422131158_UpdateTable")]
+    partial class UpdateTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/PERKIHub.RestApi/Migrations/20230422131158_UpdateTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PERKIHub.RestApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PH_ProfilePictureDef",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    File = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PH_ProfilePictureDef", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PH_ProfilePictureDef_PH_UserDef_UserID",
+                        column: x => x.UserID,
+                        principalTable: "PH_UserDef",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PH_ProfilePictureDef_UserID",
+                table: "PH_ProfilePictureDef",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PH_ProfilePictureDef");
+        }
+    }
+}
