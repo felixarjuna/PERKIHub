4185530f1eaea605330a41d77e62c2dd8603d216
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-09-30 13:48:03 +0200

    🐧 Migrate Event Page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { trpc } from "@/lib/trpc/client";
+import { CalendarPlus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { EventCard } from "./event-card";
+
 export default function Events() {
-  return <div>Welcome to events page</div>;
+  const router = useRouter();
+
+  const { data } = trpc.events.getEvents.useQuery();
+
+  return (
+    <div className="p-8 sm:px-20">
+      <div className="sm:mt-10">
+        <h1 className="text-2xl sm:text-6xl text-gradient">Events</h1>
+        <h3 className="text-xl mt-3 sm:mt-10 sm:text-4xl">This Week</h3>
+      </div>
+
+      <div className="mt-7 w-36 sm:w-44 bg-lightmaroon">
+        <div
+          className="mt-4 flex items-center gap-3 cursor-pointer button-cream w-36 sm:w-44 justify-center"
+          onClick={() => router.push("create-event")}
+        >
+          <CalendarPlus />
+          <p>New event</p>
+        </div>
+      </div>
+
+      <div className="flex gap-10">
+        {data?.events?.map((event) => (
+          <EventCard
+            key={event.id}
+            id={event.id.toString()}
+            title={event.title}
+            date={event.date}
+            speaker={event.speaker}
+            topic={event.topic}
+            participants={event.participants}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
