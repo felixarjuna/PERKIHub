9846a816cadca1e07894727ef67963c87e2d0dd4
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-10-01 10:41:20 +0200

    🦝 Delete unused pages

--- PERKIHub.UI/src/routes/Authentication/functions.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../../lib/api/contracts';
-
-export const isUser = (user: any): user is User => {
-  return user?.hasOwnProperty('firstName') && user?.hasOwnProperty('lastName');
-};
