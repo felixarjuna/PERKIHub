6ade04106171bf5ee11ea3bcdd0580b339ff5a8f
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-10-01 14:20:03 +0200

    Update env.mjs

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -1,16 +1,13 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 import "dotenv/config";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     DATABASE_URL: z.string().min(1),
-    NEXTAUTH_SECRET: process.env.NODE_ENV === "production"
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === "production" ? z.string().min(1) : z.string().min(1).optional(),
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
@@ -22,11 +19,11 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     APPLE_CLIENT_ID: z.string().min(1),
     APPLE_CLIENT_SECRET: z.string().min(1),
-    VERCEL_URL: z.string().url().optional(),
+    // VERCEL_URL: z.string().url().optional(),
     RESEND_API_KEY: z.string().min(1),
   },
   client: {
-                // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   // runtimeEnv: {
@@ -35,6 +32,6 @@ export const env = createEnv({
   // },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
-                // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
   },
 });
