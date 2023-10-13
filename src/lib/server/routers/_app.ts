7313010324edc8ac3e3d88602c91c3abcda89fd1
Felix Arjuna <felixarjuna.de@yahoo.com>
2023-10-13 14:39:13 +0200

    🐲 Add user router

--- src/lib/server/routers/_app.ts
+++ src/lib/server/routers/_app.ts
@@ -1,10 +1,12 @@
 import { computersRouter } from "./computers";
 import { router } from "../trpc";
 import { eventsRouter } from "./events";
+import { usersRouter } from "./users";
 
 export const appRouter = router({
   computers: computersRouter,
   events: eventsRouter,
+  users: usersRouter,
 });
 
 export type AppRouter = typeof appRouter;
